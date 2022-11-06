3b0444d353f21a48b7e2cb35f1778e852b8864b0
ActiveZ <activez.git@gmail.com>
2022-11-06 04:19:45 +0100

    refacto

--- src/main/java/fr/diginamic/bestioles/BestiolesApplication.java
+++ src/main/java/fr/diginamic/bestioles/BestiolesApplication.java
@@ -1,18 +1,12 @@
 package fr.diginamic.bestioles;
 
-import fr.diginamic.bestioles.entities.Animal;
-import fr.diginamic.bestioles.entities.Person;
-import fr.diginamic.bestioles.entities.Species;
 import fr.diginamic.bestioles.repositories.AnimalRepository;
 import fr.diginamic.bestioles.repositories.PersonRepository;
 import fr.diginamic.bestioles.repositories.SpeciesRepository;
-import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.List;
 
 @SpringBootApplication
 //@AllArgsConstructor // pas besoin si @Autowired
--- src/main/java/fr/diginamic/bestioles/services/PersonService.java
+++ src/main/java/fr/diginamic/bestioles/services/PersonService.java
@@ -5,7 +5,6 @@ import fr.diginamic.bestioles.repositories.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import javax.persistence.criteria.CriteriaBuilder;
 import java.util.List;
 import java.util.Optional;
 
